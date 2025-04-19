dd309c6ee272b1ae10abaa9ec847515457ad9a6d
Abhay <rajputjiabhay3002@gmail.com>
2025-04-19 11:28:15 +0530

    again

--- app.py
+++ app.py
@@ -6,30 +6,18 @@ from typing import Dict, Any
 
 app = FastAPI()
 
-
-
-
 class InputData(BaseModel):
     data: list
-
-
-
 
 @app.post("/predict")
 async def predict(input_data:InputData):
     try:
-        # Step 1: Convert JSON to DataFrame
+        
        graph = workflow()
        input = input_data.data[0]
 
        response = graph.execute(input)
 
-       
-
-        # Step 3: Make predictions using the model
-       
-
-        # Step 4: Return the predictions as JSON
        return {"predictions": response}
 
     except Exception as e:
